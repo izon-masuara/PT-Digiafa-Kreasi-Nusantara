364cb813bed74cc8c7128d81ca2baa147d406b26
kautsar_masuara <kautsarmasuara@gmail.com>
2022-12-30 08:42:31 +0800

    setup repository

---
+++ js/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const certificates = [
+    {
+        id: 1,
+        name: "Finalis Lomba Coding",
+        type: "kejuaraan",
+        publisherId: "P001",
+        publisherName: "EO Hack & Coding",
+        point: 10,
+        createdAt: '2014-10-05T14:48:00.000Z',
+    },
+    {
+        id: 2,
+        name: "Finalis Lomba Makan Kerupuk",
+        type: "kejuaraan",
+        publisherId: "P002",
+        publisherName: "Panitia 17an Desa Juara",
+        point: 20,
+        createdAt: '2010-10-05T14:48:00.000Z',
+    },
+    {
+        id: 3,
+        name: "Juara 1 Lomba Panjat Pinang",
+        type: "kejuaraan",
+        publisherId: "P002",
+        publisherName: "Panitia 17an Desa Jawara",
+        point: 30,
+        createdAt: '2022-10-05T14:48:00.000Z',
+    },
+    {
+        id: 4,
+        name: "Digital Marketing Sesi 1",
+        type: "kehadiran",
+        publisherId: "P003",
+        publisherName: "EO Pelatihan",
+        point: 5,
+        createdAt: '2014-10-05T14:48:00.000Z',
+    }
+]
