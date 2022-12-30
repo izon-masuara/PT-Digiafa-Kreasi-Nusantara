ae77616c87dbab9ff365a99c06c41388b574f161
kautsar_masuara <kautsarmasuara@gmail.com>
2022-12-30 11:16:11 +0800

    Finish to answer the questions case A

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
 'use strict'
 
+const { 
+    sortDate,
+    groupByYear,
+    view,
+    totalPoint
+} = require('./method')
+const certificates = require('./data')
+
+// Answer 1
+const dataSortByDate = sortDate(certificates)
+console.table(dataSortByDate)
+
+// Answer 2
+const groupingDataByYear = groupByYear(certificates)
+view(groupingDataByYear)
+
+// Answer 3
+const point = totalPoint(certificates)
+console.log(`\n\n Total point = ${point}\n\n`)
+
+// Answer 4
+/**
+ * Baiknya, data tersebut hanya mengandung publisherId yang kemudian
+ * id tersebut akan bereferensi ke publisherName
+ * 
+ * seperti pada contoh di bawah ini
+*/
+```
 const certificates = [
     {
         id: 1,
         name: "Finalis Lomba Coding",
         type: "kejuaraan",
         publisherId: "P001",
-        publisherName: "EO Hack & Coding",
         point: 10,
         createdAt: '2014-10-05T14:48:00.000Z',
     },
@@ -15,26 +42,18 @@ const certificates = [
         name: "Finalis Lomba Makan Kerupuk",
         type: "kejuaraan",
         publisherId: "P002",
-        publisherName: "Panitia 17an Desa Juara",
         point: 20,
         createdAt: '2010-10-05T14:48:00.000Z',
-    },
-    {
-        id: 3,
-        name: "Juara 1 Lomba Panjat Pinang",
-        type: "kejuaraan",
-        publisherId: "P002",
-        publisherName: "Panitia 17an Desa Jawara",
-        point: 30,
-        createdAt: '2022-10-05T14:48:00.000Z',
-    },
-    {
-        id: 4,
-        name: "Digital Marketing Sesi 1",
-        type: "kehadiran",
-        publisherId: "P003",
-        publisherName: "EO Pelatihan",
-        point: 5,
-        createdAt: '2014-10-05T14:48:00.000Z',
     }
 ]
+const publisher = [
+    {
+        publisherId: "P001",
+        publisherName: "EO Hack & Coding"
+    },
+    {
+        publisherId: "P002",
+        publisherName: "Panitia 17an Desa Juara"
+    }
+]
+```
